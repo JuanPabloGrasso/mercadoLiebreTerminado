cef6d4ef6760ea072f6f649428a06f99c42b849d
JuanPabloGrasso <juampigra2702@gmail.com>
2022-07-12 23:02:00 -0300

    make it better

--- app.js
+++ app.js
@@ -4,12 +4,21 @@ const path = require('path');
 const app = express();
 
 const publicPath = path.resolve(__dirname, './public');
-app.use(express.static(publicPath));
-
-app.listen(3030, () =>{
-    console.log('Servidor iniciado en: http://localhost:3030');  
-}); 
+app.use(express.static(publicPath)); 
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './views/home.html'));
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/login.html'));
+});
+
+app.get('/register', (req, res) => {
+    res.sendFile(path.join(__dirname, './views/register.html'));
+});
+
+const port = process.env.PORT || 3000;
+app.listen(port, () =>{
+    console.log('Servidor iniciado en: http://localhost:' + port);  
 });
